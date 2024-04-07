d46f3ae3d18dac44e3c1a23ca57a2e3934d17381
ajabotomey <killerbyte_93@hotmail.com>
2024-04-07 15:22:27 +1000

    Just need to refine the dialogue but otherwise done. Might add this over the original ajabotomey.github.io repo and have this as my website

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: 'Here is my PC.',
+    degrees: 'I have quite a few degrees now alongside some professional certifications.',
+    games: 'I am a lifelong gamer.',
+    projects: 'Projects',
+    bed: 'Bed',
+};
